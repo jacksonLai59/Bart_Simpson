f74b477c4e5935fd660c24c0365262c797d0c965
Wilhelm Shen <33245687+WilhelmIllust@users.noreply.github.com>
2020-09-23 14:47:47 +0800

    Initial Commit

---
+++ Demo3/Demo3App.swift
@@ -0,0 +1,17 @@
+//
+//  Demo3App.swift
+//  Demo3
+//
+//  Created by User11 on 2020/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Demo3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
